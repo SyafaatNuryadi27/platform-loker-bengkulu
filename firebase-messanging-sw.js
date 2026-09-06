c34041ee3d9489206d16dfd13729350b8c21fe89
Syafaat Nuryadi <syafaatnuryadi27@gmail.com>
2026-09-07 01:38:35 +0700

    Add files via upload

---
+++ firebase-messanging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/12.18.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.18.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCwc-W77xFztHROZEpvfK0nWZC2P9bNGOQ",
+  authDomain: "platform-loker-bengkulu.firebaseapp.com",
+  projectId: "platform-loker-bengkulu",
+  storageBucket: "platform-loker-bengkulu.firebasestorage.app",
+  messagingSenderId: "522033003669",
+  appId: "1:522033003669:web:4ff46c3f27a8df48eeaf9c"
+});
+
+firebase.messaging();
